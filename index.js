52b4949a8cadd0a7d6e8f3addd6432d8faebc2b5
MridulSheikh <prince9nazir@gmail.com>
2021-11-11 20:23:52 +0600

    new changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const app = express();
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT|| 5000;
 
 //WatchDog
@@ -21,16 +22,31 @@ async function run(){
         console.log("mongo server connect successfully")
         const database = client.db("watchDog");
         const ProductCollection = database.collection("product");
-
-        //get products 
-        app.get('/product',async(req, res)=>{
+        const ordersCollection = database.collection("orders");
+      //get products 
+         app.get('/product',async(req, res)=>{
             const cursor = ProductCollection.find({});
             const product= await cursor.toArray();
             res.send(product)
           })
+      //get single service
+      app.get('/product/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const service = await ProductCollection.findOne(query);
+        res.json(service)
+     })
+      //order service
+      app.post('/Orders', async (req, res)=>{
+        const order = req.body;
+        console.log(order)
+        const result = await ordersCollection.insertOne(order);
+        res.json(result)
+    })
+         
     }
     finally{
-
+       
     }
 }
 run().catch(console.dir);
